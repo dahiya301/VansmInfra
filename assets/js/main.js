d6039f8e488e8de8e56f10679fce4d3944b1078f
dahiya301 <sachinbeasian@gmail.com>
2026-06-27 13:00:05 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
+window.addEventListener('scroll',()=>{
 
+const nav=document.querySelector('.navbar');
+
+if(window.scrollY>50){
+nav.style.background='#111';
+}else{
+nav.style.background='transparent';
+}
+
+});
