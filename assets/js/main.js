875aca92c12828199b266e7d1f82b523a738efef
Sachin <sachin.7353@gmail.com>
2026-06-27 15:24:29 +0530

    Updated the code for whole website

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,15 +23,14 @@ prevEl:".swiper-button-prev"
 
 });
 
-window.addEventListener("scroll",()=>{
+window.addEventListener('scroll',()=>{
 
-const nav=document.querySelector(".navbar");
-
-if(window.scrollY>50){
-nav.style.background="#111";
-}
-else{
-nav.style.background="transparent";
-}
-
-});
+    const navbar=document.querySelector('.navbar');
+    
+    if(window.scrollY>50){
+    navbar.classList.add('scrolled');
+    }else{
+    navbar.classList.remove('scrolled');
+    }
+    
+    });
